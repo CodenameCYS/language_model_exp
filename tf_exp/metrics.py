f128733e252acdf58121059371e3c55386f36b90
yisheng chen <chenyisheng@hpc4.ipa.aidigger.com>
2020-12-04 19:26:18 +0800

    修正ppl计算方法

--- tf_exp/metrics.py
+++ tf_exp/metrics.py
@@ -1,5 +1,6 @@
-import torch
 import tensorflow as tf
+
+DELTA = 1e-7
 
 class Perplexity(tf.keras.metrics.Metric):
     def __init__(self, from_logits=False, **kwargs):
@@ -12,7 +13,7 @@ class Perplexity(tf.keras.metrics.Metric):
             y_pred = tf.nn.softmax(y_pred, axis=-1)
         vocab_size = y_pred.shape[-1]
         y_true = tf.one_hot(y_true, vocab_size)
-        p = tf.math.reduce_sum(y_true * y_pred, axis=-1)
+        p = tf.math.reduce_sum(y_true * y_pred, axis=-1) + DELTA
         ppl = tf.math.pow(1/tf.math.reduce_prod(p, axis=-1), 1/p.shape[-1])
         if sample_weight is not None:
             sample_weight = tf.cast(sample_weight, self.dtype)
@@ -23,10 +24,3 @@ class Perplexity(tf.keras.metrics.Metric):
 
     def result(self):
         return self.perplexity  
-
-class TorchPerplexity:
-    def __init__(self, from_logits=True):
-        self.from_logits = from_logits
-
-    def __call__(self, y_pred, y_true):
-        pass
